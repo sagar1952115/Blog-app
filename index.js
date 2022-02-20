cdf841370b9db7380b1acf4dcba2c91ddfcde967
Sagar kumar <sagarkumar1952115@gmail.com>
2022-02-20 15:20:11 +0530

    fourth commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const PORT=process.env.PORT ||5000;
 
 dotenv.config();
 app.use(express.json());
@@ -37,6 +38,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+app.listen(PORT, () => {
   console.log("Backend is running.");
 });
