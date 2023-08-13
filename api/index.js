85be935fcc933a93672a589ee1c0db0fe1d02be6
Sagar Kumar <sagarkumar1952115@gmail.com>
2023-08-13 22:01:47 +0530

    fdad

--- api/index.js
+++ api/index.js
@@ -13,10 +13,11 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(express.json());
+app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose.set("strictQuery", false);
 mongoose
