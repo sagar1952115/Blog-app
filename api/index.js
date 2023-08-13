4bdb8f10e4718e565b87eb6ddecc3d5fd1fb968e
Sagar Kumar <sagarkumar1952115@gmail.com>
2023-08-13 21:49:36 +0530

    added bodyparser

--- api/index.js
+++ api/index.js
@@ -8,12 +8,15 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose.set("strictQuery", false);
 mongoose
